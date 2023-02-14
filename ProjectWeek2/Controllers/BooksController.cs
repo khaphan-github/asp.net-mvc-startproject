33c5e074aee9856cdc17e721bbec4a8616ebc7ce
khaphan-github <phanhoangkha01@gmail.com>
2023-02-14 10:32:17 +0700

    Init

---
+++ ProjectWeek2/Controllers/BooksController.cs
@@ -0,0 +1,16 @@
+﻿using ProjectWeek2.Models.ViewModel;
+using System.Web.Mvc;
+
+namespace ProjectWeek2.Controllers
+{
+    public class BooksController : Controller
+    {
+        // GET: Books
+          
+        public ActionResult Index()
+        {
+            Book book = new Book();
+            return View(book.GetListbooks());
+        }
+    }
+}
---
+++ ProjectWeek2/Models/Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectWeek2.Models.ViewModel {
+    public class Book {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PublicYear { get; set; }
+        public double Price { get; set; }
+        public string Cover { get; set; }
+
+        public List<Book> GetListbooks() {
+            List<Book> listBooks = new List<Book>();
+            
+            for (int i = 0; i < 10; i++) {
+
+                Book newBook = new Book() {
+                    Id = Guid.NewGuid(),
+                    Title = "This is a book",
+                    Author = "Anonymus",
+                    Cover = "image",
+                    Price = 29000000,
+                    PublicYear = 2023
+                };
+
+                listBooks.Add(newBook);
+            }
+            return listBooks;
+        }
+    }
+}
